529b7a50edf740ea9ba79c81357b562b533a7e76
Amit Chandra <camit97chandra@gmail.com>
2022-10-12 20:22:17 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 const express = require('express')
 const app = express()
+const path = require('path')
 dotenv.config({path: '../config.env'});
 require("./db/conn")
 const cors = require('cors')
@@ -14,9 +15,19 @@ app.use(cors());
 // })
 const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV =="production"){
-  app.use(express.static("c-19frontend/build"));
-}
+app.use(express.static(path.join(__dirname, "../c-19frontend/build")))
+
+
+app.get("*", function(_, res){
+  res.sendFile(
+    path.join(__dirname, "../c-19frontend/build/index.html"),
+    function(err){
+      if(err){
+        res.status(500).send(err)
+      }
+    }
+  )
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)

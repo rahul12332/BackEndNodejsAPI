dea6f72d3a570fd9f285db38686f7c2e66ca576e
Amit Chandra <camit97chandra@gmail.com>
2022-10-12 22:06:34 +0530

    now

--- src/app.js
+++ src/app.js
@@ -15,10 +15,10 @@ app.use(cors());
 // })
 
 if(process.env.NODE_ENV =="production"){
-  app.use(express.static("c-19frontend/build"));
+  app.use(express.static("/c-19frontend/build"));
   const path =require("path");
   app.get("*", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'c-19frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '/c-19frontend', '../c-19frontend/build', '../c-19frontend/build/index.html'));
   })
 }
 const PORT = process.env.PORT || 5000;
